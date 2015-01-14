6f57a56d6a51767579fca2d2a3508d47884961aa
skyler <zhangning333@gmail.com>
2015-01-14 14:07:27 +0800

    fixbug

--- gitsearch.js
+++ gitsearch.js
@@ -1,37 +1,58 @@
 #!/usr/bin/env node
 
-//console.log('hello world');
-//
-//console.log(process.argv);
 
 var program = require('commander');
 var request = require('request');
+var chalk = require('chalk');
 
-program.version('0.0.1').usage('<keywords>').parse(process.argv);
+
+program
+    .version('0.0.1')
+    .usage('[options] <keywords>')
+    .option('-o, --owner [name]', 'Filter by the repositories owner')
+    .option('-l, --language [language]', 'Filter by the repositories language')
+    .option('-f, --full', 'Full output without any styling')
+    .parse(process.argv);
+
+
+console.log(program.args);
 
 if (!program.args.length) {
     program.help();
 } else {
     var keywords = program.args;
-    var url = 'https://api.github.com/search/repositories?sort=stars&order=desc&q=' + keywords;
+    var url = 'https://api.github.com/search/repositories?sort=stars&order=desc&q='+keywords;
+    if(program.owner) {
+        url = url + '+user:' + program.owner;
+    }
+    if(program.language) {
+        url = url + '+language:' + program.language;
+    }
 }
-
-//console.log (url);
 
 request({
     method : 'GET',
-    header : {
+    headers : {
         'User-Agent': 'skylerzhang'
     },
     url : url
 
 }, function(error, response, body){
-    console.log(response.statusCode);
-    console.log(body);
     if (!error && response.statusCode == 200){
         var body = JSON.parse(body);
-        console.log(body);
+        if(program.full) {
+            console.log(body);
+        } else {
+            for(var i = 0; i < body.items.length; i++) {
+                console.log(chalk.cyan.bold('Name: ' + body.items[i].name));
+                console.log(chalk.magenta.bold('Owner: ' + body.items[i].owner.login));
+                console.log(chalk.grey('Desc: ' + body.items[i].description + '\n'));
+                console.log(chalk.grey('Clone url: ' + body.items[i].clone_url + '\n'));
+            }
+            process.exit(0);
+        }
     } else if (error) {
         console.log('Error: '+ error);
+        process.exit(1);
     }
 });
